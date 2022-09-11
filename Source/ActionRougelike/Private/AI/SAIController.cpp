2f85b117785aee3d29655a00481a937de3401b16
杨童伟 <yangtw7@gmail.com>
2022-09-11 19:21:43 +0800

    Lecture 11
    学习了
    1. AI系统和行为树的入门知识。
    2. 解决一个因为在build中添加Module而引发的编译问题。
    3.调试过程中按'键可以启用gameplay debugger，调试AI很方便。

---
+++ Source/ActionRougelike/Private/AI/SAIController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SAIController.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void ASAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	RunBehaviorTree(BehaviorTree);
+
+	APawn* MyPawn =  UGameplayStatics::GetPlayerPawn(this,0);
+	if (ensure(MyPawn))
+	{
+		// 此处用的是硬编码。
+		// 在C++中定义变量，再在蓝图中使用，就可以消除硬编码。
+		GetBlackboardComponent()->SetValueAsVector("MoveToLocation",MyPawn->GetActorLocation());	
+
+		GetBlackboardComponent()->SetValueAsObject("TargetActor",MyPawn);
+	}
+}
